6eb0623311dd1efa1d82fec796d4317e4f0739fa
naghamabuwarda <nasomabuwarda@gmail.com>
2023-07-12 20:00:22 +0300

    upload images using multer relates #3

---
+++ server/middlewares/index.ts
@@ -0,0 +1,21 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'server/uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const upload = multer({
+    storage: storage, fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload a valid image file'))
+        }
+        cb(undefined, true)
+    }
+}
+);
+
+export default upload;
